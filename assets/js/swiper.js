c8b63678dbd319c43d72061c8cd0105c892ad441
Danila Sh <danila8963100@gmail.com>
2024-07-07 03:47:38 +0300

    07.07.2024

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -8,216 +8,33 @@ const swiper1 = new Swiper('.main-products__swiper', {
     prevEl: '.main-products .swiper-button-prev',
   },
 
-  // breakpoints: {
-  //   1601: {
-  //     spaceBetween: 17.75,
-  //   },
-  //   1200: {
-  //     spaceBetween: 34,
-  //   },
-  //   0: {
-  //     spaceBetween: 20, 
-  //   }
-  // }
+  breakpoints: {
+    1920: {
+      spaceBetween: 10,
+    },
+    768: {
+      spaceBetween: 10,
+    },
+    0: {
+      spaceBetween: 5, 
+    }
+  }
 });
 
-const swiper2 = new Swiper('.main-product-swiper-2', {
+const swiper2 = new Swiper('.main-articles__swiper', {
   grabCursor: true,
   slidesPerView: 'auto',
+  spaceBetween: 10,
 
-  navigation: {
-    nextEl: '.swiper-button-next-2',
-    prevEl: '.swiper-button-prev-2',
-  },
-
-  pagination: {
-    el: ".swiper-pagination-2",
-    dynamicBullets: true,
-  },
-  
-  breakpoints: {
-    1601: {
-      spaceBetween: 17.75,
-    },
-    1200: {
-      spaceBetween: 34,
-    },
-    0: {
-      spaceBetween: 20, 
+    breakpoints: {
+      1920: {
+        spaceBetween: 10,
+      },
+      768: {
+        spaceBetween: 10,
+      },
+      0: {
+        spaceBetween: 4.74, 
+      }
     }
-  }
-});
-
-const swiper3 = new Swiper('.main-product-swiper-3', {
-  grabCursor: true,
-  slidesPerView: 'auto',
-
-  navigation: {
-    nextEl: '.swiper-button-next-3',
-    prevEl: '.swiper-button-prev-3',
-  },
-
-  pagination: {
-    el: ".swiper-pagination-3",
-    dynamicBullets: true,
-  },
-  
-  breakpoints: {
-    1601: {
-      spaceBetween: 17.75,
-    },
-    1200: {
-      spaceBetween: 34,
-    },
-    0: {
-      spaceBetween: 20, 
-    }
-  }
-});
-
-const swiper4 = new Swiper('.main-product-swiper-4', {
-  grabCursor: true,
-  slidesPerView: 'auto',
-
-  navigation: {
-    nextEl: '.swiper-button-next-4',
-    prevEl: '.swiper-button-prev-4',
-  },
-
-  pagination: {
-    el: ".swiper-pagination-4",
-    dynamicBullets: true,
-  },
-  
-  breakpoints: {
-    1601: {
-      spaceBetween: 17.75,
-    },
-    1200: {
-      spaceBetween: 34,
-    },
-    0: {
-      spaceBetween: 20, 
-    }
-  }
-});
-
-const swiper5 = new Swiper('.main-product-swiper-5', {
-  grabCursor: true,
-  slidesPerView: 'auto',
-
-  navigation: {
-    nextEl: '.swiper-button-next-5',
-    prevEl: '.swiper-button-prev-5',
-  },
-
-  pagination: {
-    el: ".swiper-pagination-5",
-    dynamicBullets: true,
-  },
-  
-  breakpoints: {
-    1601: {
-      spaceBetween: 17.75,
-    },
-    1200: {
-      spaceBetween: 34,
-    },
-    0: {
-      spaceBetween: 20, 
-    }
-  }
-});
-
-const swiper6 = new Swiper('.main-swiper-6', {
-  grabCursor: true,
-  slidesPerView: 'auto',
-
-  navigation: {
-    nextEl: '.swiper-button-next-6',
-    prevEl: '.swiper-button-prev-6',
-  },
-
-  pagination: {
-    el: ".swiper-pagination-6",
-    dynamicBullets: true,
-  },
-  
-  breakpoints: {
-    1200: {
-      spaceBetween: 34,
-    },
-    0: {
-      spaceBetween: 20, 
-    }
-  }
-});
-
-const swiper7 = new Swiper(".main-swiper-7", {
-  slidesPerView: 'auto',
-  grid: {
-    rows: 2,
-    fill: "row",
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next-7',
-    prevEl: '.swiper-button-prev-7',
-  },
-
-  pagination: {
-    el: ".swiper-pagination-7",
-    dynamicBullets: true,
-  },
-
-  breakpoints: {
-    550: {
-      spaceBetween: 17,
-    },
-    0: {
-      spaceBetween: 10, 
-    }
-  }
-});
-
-const swiper8 = new Swiper(".main-swiper-8", {
-  spaceBetween: 20,
-
-  navigation: {
-    nextEl: '.swiper-button-next-8',
-    prevEl: '.swiper-button-prev-8',
-  },
-
-  pagination: {
-    el: ".swiper-pagination-8",
-    dynamicBullets: true,
-  },
-});
-
-
-const swiper9 = new Swiper(".main-swiper-9", {
-  slidesPerView: 'auto',
-  grid: {
-    rows: 2,
-    fill: "row",
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next-9',
-    prevEl: '.swiper-button-prev-9',
-  },
-
-  pagination: {
-    el: ".swiper-pagination-9",
-    dynamicBullets: true,
-  },
-
-  breakpoints: {
-    550: {
-      spaceBetween: 17,
-    },
-    0: {
-      spaceBetween: 10, 
-    }
-  }
 });
