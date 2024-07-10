5fc3d183f6720a37ba62ff46ee037f2c40069347
Danila Sh <danila8963100@gmail.com>
2024-07-10 20:59:14 +0300

    10.07.24

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -14,7 +14,7 @@ document.addEventListener('click', function(event) {
 window.onscroll = () => scrollFunction();
 
 function scrollFunction() {
-    if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && window.innerWidth <= 550) {
+    if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && window.screen.availWidth < 1070) {
         document.querySelector('.btn-scroll-top').style.display = 'flex';
         document.querySelector('.btn-scroll-top').onclick = function scrollToTop() {
             window.scrollTo({
@@ -23,18 +23,78 @@ function scrollFunction() {
             });
         }    
     } else {
-      document.querySelector('.btn-scroll-top').style.display = 'none';
+        document.querySelector('.btn-scroll-top').style.display = 'none';
     }
-
-    // if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && window.innerWidth <= 550) {
-    //     document.querySelector('.btn-scroll-top').style.display = 'flex';
-    //     document.querySelector('.btn-scroll-top').onclick = function scrollToTop() {
-    //         window.scrollTo({
-    //             top: 0,
-    //             behavior: 'smooth'
-    //         });
-    //     }    
-    // } else {
-    //   document.querySelector('.btn-scroll-top').style.display = 'none';
-    // }
 }
+
+const filter = document.querySelector('.filter');
+const filterBtn = document.querySelector('.catalog-category__filter-btn');
+const filterBtnMobile = document.querySelector('.filter-btn');
+const filterCrossBtn = document.querySelector('.filter__cross');
+const openFilter = () => {
+    if (window.screen.availWidth < 1070) {
+        document.body.classList.toggle('overflowY-hidden');
+    }
+    document.querySelector('main').classList.toggle('overlay');
+    document.querySelector('footer').classList.toggle('overlay');
+    filter.classList.toggle('filter_active');
+};
+
+filterBtn.addEventListener('click', openFilter); 
+filterCrossBtn.addEventListener('click', openFilter); 
+
+if (window.screen.availWidth < 1070) {
+    filterBtnMobile.style.display = 'flex';
+    filterBtnMobile.addEventListener('click', openFilter); 
+}
+
+window.addEventListener('resize', () => {
+    if (window.screen.availWidth < 1070) {
+        filterBtnMobile.style.display = 'flex';
+        filterBtnMobile.addEventListener('click', openFilter); 
+    } else {
+         document.querySelector('.filter-btn').style.display = 'none';
+    }
+})
+
+const paramLinks = document.querySelectorAll('.param__link');
+const filterParams = document.querySelectorAll('.filter__param');
+
+paramLinks.forEach(function(link, index) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        filterParams.forEach(function(param, paramIndex) {
+            if (paramIndex !== index && param.classList.contains('open')) {
+                param.classList.remove('open');
+            }
+        });
+
+        const filterParam = this.nextElementSibling;
+
+        if(filterParam.classList.contains('open')) {
+            filterParam.classList.remove('open');
+        } else {
+            filterParam.classList.add('open');
+        }
+    });
+});
+
+const decreaseItems = document.getElementById('decreaseItems');
+const defaultItems = document.getElementById('defaultItems');
+
+decreaseItems.addEventListener('click', () => {
+    document.querySelector('.decrease-btn').classList.add('active');
+    document.querySelector('.default-btn').classList.remove('active');
+    document.querySelector('.catalog-product__column').style.gridTemplateColumns = 'repeat(3, 1fr)';
+    document.querySelectorAll('.catalog-product-card').forEach(e => { e.style.maxWidth = '550px' });
+    document.querySelectorAll('.catalog-product-card__img-block ').forEach(e => { e.style.height = '795px' });
+});
+
+defaultItems.addEventListener('click', () => {
+    document.querySelector('.default-btn').classList.add('active');
+    document.querySelector('.decrease-btn').classList.remove('active');
+    document.querySelector('.catalog-product__column').style.gridTemplateColumns = '';
+    document.querySelectorAll('.catalog-product-card').forEach(e => { e.style.maxWidth = '' });
+    document.querySelectorAll('.catalog-product-card__img-block ').forEach(e => { e.style.height = '' });
+});
